0c16a96cd137c7b66f826c6115d31df21bb1e7a2
Mohammad Rahmani <rto1680@gmail.com>
2022-07-20 20:15:43 +0430

    Add chat tab design

--- lib/pages/home/tabs/chat_tab.dart
+++ lib/pages/home/tabs/chat_tab.dart
@@ -1,3 +1,5 @@
+import 'package:day60/data/chat.dart';
+import 'package:day60/pages/home/tabs/components/chat_widget.dart';
 import 'package:day60/pages/home/tabs/components/story_list.dart';
 import 'package:day60/shared/constants/color_constants.dart';
 import 'package:flutter/material.dart';
@@ -11,8 +13,12 @@ class ChatTab extends StatefulWidget {
 }
 
 class _ChatTabState extends State<ChatTab> {
+  final chats = getChats();
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: ColorConstants.lightBackgroundColor,
@@ -22,17 +28,36 @@ class _ChatTabState extends State<ChatTab> {
         actions: [
           IconButton(
             splashRadius: 20,
-            icon: Icon(Iconsax.search_normal, color: Colors.black, size: 22,),
+            icon: Icon(Iconsax.search_normal_1, color: Colors.black, size: 22,),
             onPressed: () {},
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 20,),
             StoryList(),
-            SizedBox(height: 20,),
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Chats", style: theme.textTheme.subtitle1?.copyWith(fontWeight: FontWeight.bold, fontSize: 18),),
+                  SizedBox(height: 8,),
+                  Column(
+                    children: chats.map((e) => Column(
+                      children: [
+                        ChatWidget(chat: e),
+                        chats.indexOf(e) != chats.length - 1 ? Divider(indent: 80,) : SizedBox(),
+                      ],
+                    )).toList(),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
